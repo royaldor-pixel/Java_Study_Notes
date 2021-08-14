baa906a8344d8d60d121179d69368b132301627f
royaldor-pixel <zyp908056771@Gmail.com>
2021-08-14 22:45:51 +0800

    Initial commit

--- src/study/RandNumberArray_2.java
+++ src/study/RandNumberArray_2.java
@@ -8,4 +8,19 @@ package study;
  * @create 2021-08-14 22:39
  **/
 public class RandNumberArray_2 {
+    public static void main(String[] args) {
+        int [] randArray = new int[6];
+        for (int i = 0; i < randArray.length; i++) {
+            randArray[i]=(int) (29*Math.random()) + 1;
+            for (int j = 0; j < i; j++) {
+                if(randArray[i] == randArray[j]){
+                    i--;
+                    break;
+                }
+            }
+        }
+        for (int j : randArray) {
+            System.out.print(j + '\t');
+        }
+    }
 }
